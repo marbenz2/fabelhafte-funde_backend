db680da9d061b54779aec09b9cab484a08677325
marbenz2 <benzinger.marcel@gmail.com>
2024-07-08 23:04:35 +0200

    add access control

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -6,8 +6,30 @@ export const Users: CollectionConfig = {
     useAsTitle: 'email',
   },
   auth: true,
+  access: {
+    create: ({ req }) => req.user?.role === 'admin',
+    read: ({ req }) => req.user?.role === 'admin' || req.user?.role === 'user',
+    update: ({ req }) => req.user?.role === 'admin',
+    delete: ({ req }) => req.user?.role === 'admin',
+  },
   fields: [
     // Email added by default
     // Add more fields as needed
+    {
+      name: 'role',
+      type: 'select',
+      options: [
+        {
+          label: 'Admin',
+          value: 'admin',
+        },
+        {
+          label: 'User',
+          value: 'user',
+        },
+      ],
+      defaultValue: 'user',
+      required: true,
+    },
   ],
 }
