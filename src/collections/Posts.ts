2ea417fa15e41435540d30d5b63742fedc084a40
marbenz2 <benzinger.marcel@gmail.com>
2024-07-08 15:09:13 +0200

    add posts collection

---
+++ src/collections/Posts.ts
@@ -0,0 +1,15 @@
+import type { CollectionConfig } from 'payload'
+
+export const Posts: CollectionConfig = {
+  slug: 'posts',
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'test',
+      type: 'text',
+      required: true,
+    },
+  ],
+}
